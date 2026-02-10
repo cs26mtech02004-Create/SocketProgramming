9cdf62c9899b6a93ac6912edde6deafee3a3c005
cs26mtech02004-Create <cs26mtech02004@iith.ac.in>
2026-02-10 19:35:11 +0530

    Add files via upload

---
+++ FileHandlingSocket.py
@@ -0,0 +1,52 @@
+import os
+import my_enc_decrp as ENC_DECRP
+
+def filemgmtcommand(cmd, dh_secretkey, reqd_filename=None):
+    if cmd == "LIST":
+        print("--- Directory Listing ---")
+        lists = []
+        for file in os.listdir("./"):
+            lists.append(file)
+        print(lists)
+        return "list found see SERVER LOGS"
+
+    if cmd not in ("GET", "INFO", "SIZE","LIST"):
+        return f"Unknown command: {cmd}"
+
+    if not reqd_filename or not os.path.exists(reqd_filename):
+        print(f"Error: File '{reqd_filename}' not found.")
+        return ENC_DECRP.aes_encrypt("file not found".encode(), dh_secretkey)
+
+    match cmd:
+        case "GET":
+            seq_no = 0
+            packets = []
+            print(f"--- Content of {reqd_filename} ---")
+            with open(reqd_filename, "rb") as f:
+                while True:
+                    chunk = f.read(1024)
+                    if not chunk:
+                        break
+                    cipher_data = ENC_DECRP.aes_encrypt(chunk, dh_secretkey)
+                    mac = ENC_DECRP.compute_mac(cipher_data, seq_no, dh_secretkey)
+                    header = f"{seq_no}|{len(cipher_data)}|\n".encode()
+                    packet = header + cipher_data + mac
+                    packets.append(packet)
+                    seq_no += 1
+                packets.append(b"EOF")
+            print("chunks sended from filemgmtcommand")
+            return packets
+
+        case "INFO":
+            print(f"--- Stats for {reqd_filename} ---")
+            info = str(os.stat(reqd_filename)).encode()
+            return ENC_DECRP.aes_encrypt(info, dh_secretkey)
+
+        case "SIZE":
+            size = os.stat(reqd_filename).st_size
+            sizeinfo = f"File size: {size} bytes".encode()
+            return ENC_DECRP.aes_encrypt(sizeinfo, dh_secretkey) #send kiya encyption byte data server ko
+
+
+
+# print(filemgmtcommand("LI","credentials.txt"))
---
+++ my_enc_decrp.py
@@ -0,0 +1,39 @@
+import hashlib
+import hmac
+import os
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+
+def aes_encrypt(message, dh_secretkey):
+    key = hashlib.sha256(str(dh_secretkey).encode()).digest()
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
+    cipher_data = aesgcm.encrypt(nonce, message, b"")
+    return nonce + cipher_data
+
+def aes_decrypt(encrypted_data, dh_secretkey):
+    key = hashlib.sha256(str(dh_secretkey).encode()).digest()
+    aesgcm = AESGCM(key)
+    nonce = encrypted_data[:12]
+    cipher_text = encrypted_data[12:]
+    plaintext_bytes = aesgcm.decrypt(nonce, cipher_text, b"")
+    return plaintext_bytes
+
+def verify_mac(data, seq_no, dh_secretkey, received_mac):
+    key = hashlib.sha256(str(dh_secretkey).encode()).digest()
+    msg = seq_no.to_bytes(4, "big") + data
+    expected_mac = hmac.new(key, msg, hashlib.sha256).digest()
+    return hmac.compare_digest(expected_mac, received_mac)
+
+
+def compute_mac(plain_data, seq_no, dh_secretkey):
+    key = hashlib.sha256(str(dh_secretkey).encode()).digest()
+    msg = seq_no.to_bytes(4, "big") + plain_data
+    return hmac.new(key, msg, hashlib.sha256).digest()
+
+
+# ---- Test k liye data h----
+# edata = aes_encrypt(b"hello world!", b"0102030405060708")
+# print(edata)
+#
+# print(aes_decrypt(edata, b"0102030405060708"))
